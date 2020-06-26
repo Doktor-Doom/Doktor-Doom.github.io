2a34c9e7601328720c2c20c49865608f6bc1f3df
Doktor-Doom <andre.o.martin@gmail.com>
2020-06-26 14:19:56 -0700

    initial commit

--- Javascript.js
+++ Javascript.js
@@ -14,29 +14,25 @@ if (hourNow > 18) {
 
 document.write('<p>' + greeting + '</p>');
 
-<<p>CLick for something to happen?</p>
-<button onclick="myFunction()">Try it</button>
- <p id="demo"></p>
 
- <script>
+
+
+
 function myFunction() {
                 var person = prompt("Please enter your name", "Harvey Dent");
                 if (person != null) {
                     document.getElementById("demo??").innerHTML = "Is that you, Harvey?" + person + "! TWO-FACE!!";
-                {"}"}
-            {"}"}
-</script>
+                }
+}
 
 
-<p>CLick the button to wait 3 seconds, then alert "Intruder Alert".</p>
 
-<button onclick="myFunction()">Try it</button>
 
-<script>
+
 function myFunction() {
     setTimeout(function(){ alert("Intruder Alert"); }, 3000);
 }
     
-}</script>
+
 
 
